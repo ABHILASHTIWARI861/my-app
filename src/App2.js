7e68452f3bd323c9f79b819d843a8a94ec4e1618
abhilash tiwari <abhilashtiwari.smn@gmail.com>
2025-09-13 10:45:04 +0530

    props_logic_and_flexbox

---
+++ src/App2.js
@@ -0,0 +1,17 @@
+//Agar props use nhi krte to aise krna hota naya file banana padta.
+
+// import React from "react";
+
+// function APP2(){
+//     return(
+//     <div className="card">
+//      <h2>abhilash</h2>
+//      <img src="https://imgs.search.brave.com/dvlGvqvt64YnvNnggwNDhBU35a2gioYKGGWoSRp0zo4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE3/NTUyNzgzMzg4OTEt/ZThkODQ4MWZmMDg3/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjEuMCZp/eGlkPU0zd3hNakEz/ZkRCOE1IeG1aV0Yw/ZFhKbFpDMXdhRzkw/YjNNdFptVmxaSHc1/Zkh4OFpXNThNSHg4/Zkh4OA"
+//       alt="Beyonce"
+//      />
+//       <p>+91-674483647</p>
+//      <p>dgddgv.smn@gmail.com</p>
+//     </div> 
+//     )
+// }
+// export default APP2;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App.js";
+// import App2 from "./App2.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root=ReactDom.createRoot(document.querySelector(".root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div className="main">
+  <h1>My contact List</h1>,
+  <div className="container">
+<App
+  name="ABHILASH"
+  img="https://imgs.search.brave.com/dvlGvqvt64YnvNnggwNDhBU35a2gioYKGGWoSRp0zo4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE3/NTUyNzgzMzg4OTEt/ZThkODQ4MWZmMDg3/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjEuMCZp/eGlkPU0zd3hNakEz/ZkRCOE1IeG1aV0Yw/ZFhKbFpDMXdhRzkw/YjNNdFptVmxaSHc1/Zkh4OFpXNThNSHg4/Zkh4OA"
+  phone="+91-674483647"
+  email="abhi@gmail.com"
+/>
+<App
+  name="BEYONCE"
+  img="https://imgs.search.brave.com/899NlYVSvoFRTQkw5VMxIPuBmn1zvr7Ex0xHcLX1CHk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWd2/My5mb3Rvci5jb20v/aW1hZ2VzL2NvdmVy/LXBob3RvLWltYWdl/L0JsdXJyeS1mZW1h/bGUtcG9ydHJhaXQt/YmVmb3JlLWFuZC1h/ZnRlci1waG90by1l/bmhhbmNlbWVudC11/c2luZy1Gb3Rvci1B/SS1waG90by1lZGl0/b3Itb25saW5lLmpw/Zw"
+  phone="+91-674483648"
+  email="asdf@gmail.com"
+/>
+<App
+  name="ARIJIT SINGH"
+  img="https://imgs.search.brave.com/2U-9epoJ1xiRxUGYXS4Rj0KjJB9KhtcN0CI13DzvRDk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudG9paW1nLmNv/bS90aHVtYi9pbWdz/aXplLTIzNDU2LG1z/aWQtODIyMzcyNDQs/d2lkdGgtNjAwLHJl/c2l6ZW1vZGUtNC84/MjIzNzI0NC5qcGc"
+  phone="+91-674483649"
+  email="arijit2@gmail.com"
+  />
+  <App
+  name="TAYLOR SWIFT"
+  img="https://imgs.search.brave.com/be8jak_c_dpMUuB7T7GRMLqlqQWhC1DjMLAiobwvpKE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2QzLzlh/L2Y3L2QzOWFmNzIy/ZWY4MmIwZDhlMGFj/ZjRiMGVmNDZjNmY5/LmpwZw"
+  phone="+91-674483650"
+  email="amit32@gmail.com"
+  />
+  <App
+  name="DILJIT DOSANJH"
+  img="https://imgs.search.brave.com/vwBWgLZrQJKCXIVDqNXmHrMCRNQGS5lGp4wl0CrNbGg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbS5y/ZWRpZmYuY29tL25l/d3MvMjAyNC9kZWMv/MTJkaWxqaXQuanBn"
+  phone="+91-674483651"
+  email="dilji32@gmail.com"
+  />
+  <App
+  name="JONAS BROTHERS"
+  img="https://imgs.search.brave.com/TSy1mb2Dq0aASWi1Mpm_k5Yr0Wj0gRS13hQGZVCQm4Y/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wZW9w/bGUuY29tL3RobWIv/dzAtVUpiODNDVkJC/WVdXS0xGNXA0VGxa/ZVNNPS80MDAweDAv/ZmlsdGVyczpub191/cHNjYWxlKCk6bWF4/X2J5dGVzKDE1MDAw/MCk6c3RyaXBfaWNj/KCk6Zm9jYWwoOTk5/eDA6MTAwMXgyKS9q/b25hcy1icm90aGVy/cy0zLWNhY2U1ZjIx/M2Q3ZTQzMWViM2Nh/ZmE5OGY3NzBhM2Ez/LmpwZw"
+  phone="+91-674483652"
+  email="jonas2@gmail.com"
+  />
+  
+</div>
+  </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
